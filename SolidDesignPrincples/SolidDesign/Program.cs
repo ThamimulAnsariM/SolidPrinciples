dc155daebbf503d39ebab5c70bfeb1abac6966e5
ThamimulAnsariM <154893420+ThamimulAnsariM@users.noreply.github.com>
2025-01-22 18:07:35 +0530

    Add files via upload
    This repository contains practical examples and code snippets demonstrating the implementation of the SOLID principles in .NET C#. Each principle is showcased with clear and concise examples to illustrate how to design robust, maintainable, and scalable object-oriented applications. Ideal for developers looking to enhance their understanding of clean code practices in real-world scenarios.
    
    Key Features:
    --------------
    Single Responsibility Principle (SRP) with examples.
    Open/Closed Principle (OCP) demonstrations.
    Liskov Substitution Principle (LSP) implementation.
    Interface Segregation Principle (ISP) examples.
    Dependency Inversion Principle (DIP) with dependency injection.
    Usage:
    -------
    =>Clone the repository and explore individual principle examples in organized folders.
    =>Each principle includes comments and documentation for easy understanding.
    =>Use these examples as a reference for your own projects to improve design patterns and code quality.

---
+++ SolidDesignPrincples/SolidDesign/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidDesign
+{
+    internal class Program
+    {
+        /// <summary>
+        /// a. out vs ref is c# keyword
+        ///b. ref is two way from caller and calle and back.
+        //c. out is one way send data back from calle to caller.
+        /// </summary>
+        /// <param name="somevalue"></param>
+        public static void reffunction(ref int somevalue)//calle
+        {
+            somevalue += somevalue;
+        }
+
+        public static void outfntion(out int somevalue) //calle
+        {
+            somevalue= 0;
+            somevalue += somevalue;
+        }
+        static void Main(string[] args)
+        {
+
+            string rever_str = "marimuthu";
+            string Afterrever= new string(rever_str.Reverse().ToArray());
+            object ac = "mari";
+            object bc = new string("mari".ToCharArray());
+            if (ac.Equals(bc))
+            {
+                //true
+            }
+
+            if (ac==bc)
+            {
+                //false going
+            }
+            Singleton singleton = Singleton.instance;
+
+            //protectconstructor ct=new protectconstructor();
+            //protected constructor does not allow to create object fot base class = new child class
+            //protectconstructor testprotconstructor = new testprotconstructor();
+
+            #region get the repeat letter  count and select the letter
+
+            string repeat = "hello world";
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            foreach (char data in repeat)
+            {
+                if (char.IsLetter(data))
+                {
+                    if (map.Keys.Where(x => x == data).Any() == true)
+                    {
+                        map[data] = map[data] + 1;
+                    }
+                    else
+                    {
+                        map.Add(data,1 );
+                    }
+                }
+            }
+
+            var maxvlaus = map.FirstOrDefault(x => x.Value == map.Values.Max());
+            Console.WriteLine($"the most repeat letter is {maxvlaus.Key} and count is {maxvlaus.Value}");
+            #endregion
+
+            Console.ReadKey(); 
+            int a = 10;
+            Console.WriteLine("using ref a values before method call", a);
+            reffunction(ref a); //caller 
+            Console.WriteLine("using ref a values is",a);
+            int b = 20;
+            Console.WriteLine("using out a values before method call", b);
+            outfntion(out b); //caller
+            Console.WriteLine("using out a values before method call", b);
+            EmployeeLSP employeeLSP = new EmployeeLSP();
+            permanenteployee permanenteployee  =  new permanenteployee();
+            contracteployee  contracteployee = new contracteployee();
+           // contracteployee.calculatebonous();
+        }
+    }
+}
